f49ced4e4345cc0221f4b65cbfe816c4e5138391
BananasAmIRite <jasony1230@gmail.com>
2024-12-20 11:55:33 -0500

    added test line and mistake

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -7,6 +7,8 @@ import frc.robot.subsystems.ExampleSubsystem;
 /** An example command that uses an example subsystem. */
 public class ExampleCommand extends Command {
   private final ExampleSubsystem m_subsystem;
+      private String someName = "hello! this is a test..."; 
+      oops i made a mistake!
 
   /**
    * Creates a new ExampleCommand.
